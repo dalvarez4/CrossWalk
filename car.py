0c983f2532d23282797eddb85ecba9deb4042167
Daniel Alvarez <dalvarez@mines.edu>
2021-11-17 21:51:11 -0700

    Car module and main have been updated

---
+++ car.py
@@ -0,0 +1,23 @@
+#this file will be used to write the functions of the car and its characteristics
+blockLength=330
+crosswalkWidth=24
+streetWidth=46
+
+class car:
+
+    #car charecteristics:
+    carLength = 9
+    carAccel = 10
+    carPosition=0
+
+
+
+
+    def __init__(self,speed,direction):
+        self.speed=speed*5280/3600
+        if direction==forward:
+
+    #
+
+    # methods
+    def carStates(self):
--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import car
+blockLength=330
+crosswalkWidth=24
+streetWidth=46
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    car1=car.car(20)
+    print(car1.carLength)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
