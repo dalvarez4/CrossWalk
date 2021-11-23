36e8e426042542a942f7e9d06a8264394cfcfe08
jsims453 <jsims@mymail.mines.edu>
2021-11-23 15:05:57 -0700

    updates

--- events.py
+++ events.py
@@ -31,6 +31,9 @@ class node:
         return self.event < other.event
 
 class event_list:
+
+    count_iter = 0
+
     def __init__(self):
         self.heap = np.empty(10, dtype=event)
         self.size_inc = 10
@@ -55,6 +58,8 @@ class event_list:
         parent_index = self.parent(index)
         while parent_index >= 0 and self.heap[parent_index] > self.heap[index]:
             #print(self.heap[index], self.heap[parent_index])
+            event_list.count_iter += 1
+            #print(event_list.count_iter)
             to_swap = self.heap[parent_index]
             self.heap[parent_index] = self.heap[index]
             self.heap[index] = to_swap
@@ -69,6 +74,7 @@ class event_list:
         r = self.child_r(index)
         l = self.child_l(index)
         while (l < len(self.heap) and self.heap[l] != None and self.heap[index] > self.heap[l]) or (r < len(self.heap) and self.heap[r] != None and self.heap[index] > self.heap[r]):
+            event_list.count_iter += 1
             to_swap = None
             if self.heap[r] == None or self.heap[l] < self.heap[r]:
                 to_swap = l
@@ -108,15 +114,82 @@ class event_list:
 
 #event_array = np.array([test_event, test_event, test_event], dtype = event)
 #print(np.pad(event_array, [(0, 3)], mode = 'constant', constant_values = None))
-#test_event = event("test", 5)
+
+#test_event = event("test", Uniform(0, 50))
 #test_list = event_list()
 #test_list.insert(test_event)
 #print(test_list)
 #events = [event("test", 20), event("test", 30), event("test", 60), event("test", 20), event("test", 1)]
-#for event in events:
-#    test_list.insert(event)
-#    print(test_list)
+
+#import random
+#def Uniform(a = 2.6,b = 4.1, x = -1):
+#    if x == -1:
+#        x = random.random()
+#    return a + (b - a) * x
 #
-#for event in events:
-#    test_list.next()
-#    print(test_list)
+#
+#num_runs = 500
+#
+#
+#num = 500
+#avg_iter = 0
+#
+#all_inserts = []
+#all_nexts = []
+#
+#inserts = []
+#nexts = []
+#elements = []
+##for event in events:
+#for run in range(5, num_runs):
+#    elements.append(run)
+#    test_event = event("test", Uniform(0, 50))
+#    test_list = event_list()
+#    test_list.insert(test_event)
+#    print(run)
+#    inserts = []
+#    nexts = []
+#    for x in range(5, 500):
+#        event_list.count_iter = 0
+#        test_list.insert(event("test", Uniform(0, 50)))
+#        avg_iter = avg_iter + (1/(x + 1)) * (event_list.count_iter - avg_iter)
+#        inserts.append(avg_iter)
+#        #print(event_list.count_iter)
+#        #print(test_list)
+#    #
+#    all_inserts.append(inserts)
+#
+#    avg_iter = 0
+#    for x in range(5, 500):
+#        event_list.count_iter = 0
+#        test_list.next()
+#        avg_iter = avg_iter + (1/(x + 1)) * (event_list.count_iter - avg_iter)
+#        nexts.append(avg_iter)
+#    #    print(test_list)
+#    all_nexts.append(nexts)
+#
+##print(len(inserts), len(nexts), len(elements))
+#avg_inserts = []
+#avg_nexts = []
+#
+#for x in range(5, 500):
+#    x = x - 5
+#    avg_insert = 0
+#    avg_next = 0
+#    for i in range(len(all_inserts)):
+#        avg_insert = avg_insert + (1/(i + 1)) * (all_inserts[i][x] - avg_insert)
+#        avg_next = avg_next + (1/(i + 1)) * (all_nexts[i][x] - avg_next)
+#    avg_inserts.append(avg_insert)
+#    avg_nexts.append(avg_next)
+#
+
+
+import matplotlib.pyplot as plt
+
+plt.scatter(elements, avg_inserts)
+
+plt.show()
+
+plt.scatter(elements, avg_nexts)
+
+plt.show()
