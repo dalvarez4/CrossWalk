24a8395186ceeb504fba38557e94081b247b6a26
Jeff <jsims@mymail.mines.edu>
2021-11-23 21:58:53 -0700

    pedestrian rework

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ if __name__ == '__main__':
         time = event.arrival_time
         sec_until_green_exp = sec_until_green_exp - (time - last_time)
         if event.name == 'ped_spawn':
-            curr_ped = ped.ped(time, event, event_list, total_peds, ped.Uniform(x = ped_speed), button_dist)
+            curr_ped = ped.ped(time, total_peds, ped.Uniform(x = ped_speed), button_dist)
             #print(curr_ped.speed)
             heappush(event_list, events.ped_event("at_button", curr_ped.button_time, curr_ped.id))
             peds[total_peds] = curr_ped
@@ -132,11 +132,11 @@ if __name__ == '__main__':
                     exit("Pedestrian trace ended prematurely")
                 heappush(event_list, events.event("ped_spawn", ped.Exponential(60 / (2*lambda_p), x = ped_arr) + time))
 
-        elif event.name == "ped_exit":
-            peds_crossed += 1
-            new_delay = peds.pop(event.id).update(event.name, time, event_list)
-            #ped_delay_mu = ped_delay_mu + (1/(peds_crossed)) * (new_delay - ped_delay_mu)
-            ped_delay_mu = updateMean(ped_delay_mu,new_delay, peds_crossed)
+        #elif event.name == "ped_exit":
+        #    peds_crossed += 1
+        #    new_delay = peds.pop(event.id).update(event.name, time, event_list)
+        #    #ped_delay_mu = ped_delay_mu + (1/(peds_crossed)) * (new_delay - ped_delay_mu)
+        #    ped_delay_mu = updateMean(ped_delay_mu,new_delay, peds_crossed)
         elif event.name == "car_spawn":
             curr_car = car.car(ped.Uniform(25, 35, x=car_speed), time)
             cars.append(curr_car)
@@ -148,9 +148,34 @@ if __name__ == '__main__':
                     exit("Auto trace ended prematurely")
                 heappush(event_list, events.event("car_spawn", ped.Exponential(60 / (2*lambda_c), x = car_arr) + time))
         elif event.name == "r_exp":
+            #reset the amount of peds crossing
+            ped.ped.peds_crossing = 0
+            #ready for a new signal press
+            ped.ped.pushed = False
             sec_until_green_exp = 35
             car_delay_mu,car_delay_sigma,cars_passed=lightHandles("Red",time,car_delay_sigma,car_delay_mu,cars_passed)
         elif event.name == "y_exp":
+            #make a copy of the list so it isnt affected by changes to the heap made during the crossing
+            waiting_peds = ped.ped.button_arrivals.copy()
+            still_waiting = []
+            for x in range(len(ped.ped.button_arrivals)):
+                #print(pedestrian.id)
+                pedestrian = heappop(waiting_peds)
+                event_list = pedestrian.walk_signal(time, event_list, sec_until_green_exp)
+                if pedestrian.walked:
+                    #update the mean
+                    ped_delay_mu = updateMean(ped_delay_mu, pedestrian.total_delay, peds_crossed)
+                    #update the amount that have crossed
+                    peds_crossed += 1
+                    #remove from the dict of active peds
+                    peds.pop(pedestrian.id)
+                else:
+                    #keep them in the list
+                    still_waiting.append(pedestrian)
+            #make new heap with remaining peds
+            ped.ped.button_arrivals = still_waiting
+            heapify(ped.ped.button_arrivals)
+
 
             car_delay_mu,car_delay_sigma,cars_passed=lightHandles("Yellow",time,car_delay_sigma,car_delay_mu,cars_passed)
             #stranded peds are waiting for the next red not the current one
@@ -160,17 +185,25 @@ if __name__ == '__main__':
             car_delay_mu,car_delay_sigma,cars_passed=lightHandles("Green",time,car_delay_sigma,car_delay_mu,cars_passed)
             #pushed button during yellow light
             sec_until_green_exp == 35 + 18 + 8
+        elif event.name == "at_button":
+            if event.id in peds.keys():
+                event_list = peds[event.id].at_signal(time, event_list, sec_until_green_exp)
+        elif event.name == "impatient":
+            if event.id in peds.keys():
+                event_list = peds[event.id].impatient_press(time, event_list, sec_until_green_exp)
         #check if its a single ped event otherwise
         #update peds in the order they arrived
-        if isinstance(event, events.ped_event):
-            if event.id in peds.keys():
-                peds[event.id].update(event.name, time, event_list, signal_left = sec_until_green_exp)
-        elif len(list(peds.keys())) > 0:
-            '''need to check the order that they make it to the button, not spawn in '''
-            keys = list(peds.keys())
-            keys.sort()
-            for key in keys:
-                event_list = peds[key].update(event.name, time, event_list, signal_left = sec_until_green_exp)
+        #if isinstance(event, events.ped_event):
+        #    if event.id in peds.keys():
+        #        peds[event.id].update(event.name, time, event_list, signal_left = sec_until_green_exp)
+        #elif len(ped.ped.button_arrivals) > 0:
+        #    '''need to check the order that they make it to the button, not spawn in '''
+        #    #for key in keys:
+            #    event_list = peds[key].update(event.name, time, event_list, signal_left = sec_until_green_exp)
+        #    heap_copy = ped.ped.button_arrivals.copy()
+        #    for pedestrian in heap_copy:
+        #        event_list = pedestrian.update(event.name, time, event_list, signal_left = sec_until_green_exp)
+
     print(f"OUTPUT Average Car Delay {car_delay_mu} Car Delay Standard Deviation {car_delay_sigma/arrivals} Average Pedestrian Delay {ped_delay_mu}")
     #print("OUTPUT ",)
     exit(0)
--- ped.py
+++ ped.py
@@ -10,18 +10,6 @@ def Uniform(a = 2.6,b = 4.1, x = random.random()):
 def Exponential(mew, x = random.random()):
     return -mew * np.log(1 - x)
 
-
-
-traffic_signals = ['r', 'y', 'g']
-
-ped_signals = ['w', 'nw']
-
-#target values
-
-d_a = 0
-
-d_p = 0
-
 #geometry constants
 
 traffic_sources = 2
@@ -41,46 +29,6 @@ RED = 18
 YELLOW = 8
 GREEN = 35
 
-#rate per minute
-lambda_p = 3
-lambda_a = 4
-
-#mph
-#auto speed
-v_j = Uniform(25, 35)
-#ft/s/s
-#auto acceleration
-a = 10
-#ft/s
-#ped speed
-v_k = Uniform(2.6, 4.1)
-#pedestrians accelerate instantly
-#autos only accelerate if delayed
-
-#state stuff
-signal_clock_remainder = 0
-
-def b_j(v_j, a):
-    return v_j**2/(2*a)
-def t_j(v_j, a):
-    return v_j / a
-def h_j(e_j, B, S, w, b_j, v_j, t_j):
-    return e_j + ((7/2*B + 3*S - w/2 - b_j) / v_j) + t_j
-def can_cross(speed, distance, crossed):
-    if crossed >= 20:
-        return False
-    return (distance + cw_peds) / speed <= signal_clock_remainder
-def will_press(peds_waiting, pushed, trace, impatient = False, stranded = False):
-    if pushed:
-        return False
-    if impatient:
-        return True
-    x = float(trace.readline())
-    #peds waiting -1?
-    if peds_waiting == 0 or stranded:
-        return Uniform(0, 16, x) <= 15
-    else:
-        return Uniform(0, peds_waiting + 1, x) <= peds_waiting
 
 class ped:
 
@@ -95,38 +43,45 @@ class ped:
     #order that peds arrive at button
     #sort by order they WILL arrive at button
     button_arrivals = []
+    #position of the button
+    button_pos = B/2 + S + B/2
 
-    def __init__(self, time, event, event_list, num_peds, speed, button_trace):
+    def __init__(self, time, num_peds, speed, button_trace):
+        #initialize the trace file to use for calculating button press probabilities, should be used regardless of whther or not a cycle is already queued I think
         if ped.button_trace == []:
             ped.button_trace = button_trace
-        #distance until they are at the button
-        #unsure what the width of the side streets are, assume side streets are same width as the main street as only one street width is stated
-        #have to cross a half block, side street, and another half block to reach the button
-        self.button_pos = B/2 + S + B/2
+            heapify(ped.button_arrivals)
+        #the time the ped starts waiting at the button, stays None if they make an early cycle
         self.delay_start = None
-        #self.speed = Uniform(2.6, 4.1)
+        #speed of the pedestrian
         self.speed = speed
-        #just gonna worry about spawning on one side for now since peds don't collide
-        #might want to worry about the next spawn event in the main loop to ensure that ID's stay unique
-        #self.next_ped = time + Exponential(2 * lambda_p)
+        #the last time the pedestrian moved
         self.last_time = time
-        #pedestrians spawn across one street and one half block, block width = B, width of street = S
-        #calculate time for button event
-        self.button_time = self.button_pos / self.speed
-        #event_list.insert(events.ped_event("at_button", self.button_time, self.id))
+        #whether or not the pedestrian is left behind during a cycle
+        self.stranded = False
+        #time the ped will reach the signal button
+        self.button_time = (ped.button_pos / self.speed) + time
+        #add them to the heap, should be sorted by their button press time
+        heappush(ped.button_arrivals, self)
+        #unique identifier for the current ped
         self.id = num_peds
+        #whether or not the ped has already walked across the cross walk
         self.walked = False
+        #whether or not the ped is at the button
         self.at_button = False
+        #curent position of the ped, or at least current as of self.last_time
         self.pos = 0
+        #the calculated total delay of the ped
         self.total_delay = 0
-        #need to determine whther or not they will make the walk cycle if there is one
-        #add ped_impatiant for one minute after arriving at button
-        #newly spaned peds should still be considered for walking? Prolly impossible for them to cross the total distance in time.
-        #event_list = self.update(event, time, event_list)
+    
+    def __lt__(self, other):
+        return self.button_time < other.button_time
 
     def push_button(self, time, event_list, signal_left):
-        #assuming cross walk works instantanuosly on a stale (with yellow delay)
-        #print(signal_left)
+        #a ped has pushed of the button, don't want a bunch of stacked walked signals
+        ped.pushed = True
+        #assuming cross walk works instantanuosly on a stale green (with yellow delay)
+        #negative signal time indicates the signal could have changed earlier in the simulation but had no reason to
         if(signal_left <= 0):
             heappush(event_list, events.event("g_exp", time))
             heappush(event_list, events.event("y_exp", time + 8))
@@ -137,92 +92,79 @@ class ped:
             heappush(event_list, events.event("r_exp", time + signal_left + 18))
         return event_list
     
-    def update(self, event, time, event_list, signal_left = 0):
-        # AUTO_ARRIVAL, PED_ARRIVAL, PED_AT_BUTTON, PED_IMPATIENT, GREEN_EXPIRES, YELLOW_EXPIRES, RED_EXPIRES, AUTO_EXIT and PED_EXIT.
-        self.pos = self.speed * (time - self.last_time) + self.pos
-        if self.pos >= self.button_pos:
-            self.pos = self.button_pos
-        if event == 'y_exp':
-            #reset crosswalk status
-            ped.pushed = False
-            #should be allowed to walk even if not right at button, anywhere within that 24 foot width of the crosswalk
-            '''only first 20 pedestrians that can make it will cross determine who is checked as available to cross in main loop'''
-            '''first 20 I'm assuming is first 20 to spawn in who can make it, not necessarily the first 20 people who make it to the button itself'''
-            #time they finished crossing if applicable
-            crossed_at = None
-            #check where the pedestrian currently is
-            #button is in the middle of the crosswalk so they just need to be within twelve feet of the button to be at the crosswalk
-            if ped.peds_crossing <=20 and (self.at_button or self.button_pos - self.pos <= 12):
-                #anyone at the button or at the crosswalk should be allowed to walk
-                self.walked = True
-                crossed_at = S / self.speed
-            elif (self.button_pos - self.pos - 12) / self.speed <= RED:
-                #if they can make it to the crosswalk and also cross within the signal time they can walk
-                self.walked = True
-                crossed_at = (self.button_pos - self.pos - 12) / self.speed + S / self.speed
 
-            if self.walked:
-                #for peds no longer waiting reduce the count
-                ped.peds_waiting -= 1
-                ped.peds_crossing += 1
-
-                #if can walk add a ped exit event
-                heappush(event_list, events.ped_event("ped_exit", crossed_at, self.id))
-
-                #calculate delay if allowed to walk
-                if self.delay_start != None:
-                    self.total_delay = time - self.delay_start
-            elif not self.walked and ped.peds_crossing > 20 and will_press(ped.peds_waiting, ped.pushed, trace = ped.button_trace, stranded=True):
+    def walk_signal(self, time, event_list, signal_left):
+        #either crosses or is stranded
+        '''should be called in order of arrival at the button'''
+        #calculate new posistion of pedestrian if not already at the button
+        if self.pos != ped.button_pos:
+            self.pos = ((time - self.last_time) * self.speed) + self.pos
+        #were waiting and then crossed
+        if ped.peds_crossing <= 20 and self.at_button:
+            #cross
+            self.crossing(time)
+        #crossed before reaching the button
+        elif ped.peds_crossing <= 20 and ((ped.button_pos - self.pos - 12 + cw_peds) / self.speed <= RED):
+            #cross without getting to the button
+            self.crossing(time)
+        #strandedonly if at button
+        #need to make sure I account for people that WILL be stranded ie, they will walk up to the light during the signal and not make it
+        #this will not negate their normal button arrival chance at pressing, ie they will have to chances to press
+        elif ped.peds_crossing > 20 and (((ped.button_pos - self.pos - 12 + cw_peds) / self.speed <= RED) or self.at_button):
+            self.stranded = True
+            #if they pushed the button make em push the button
+            if self.will_press():
                 event_list = self.push_button(time, event_list, signal_left)
-
-        if event == 'r_exp' or event == 'g_exp':
-            '''this will be anytime the light is not red'''
-            #reset the count of peds that have crossed
-            ped.peds_crossing = 0
-
-
-        if event == "at_button":
-            if self.walked:
-                #do nothing if already walked
-                return event_list
-            self.at_button = True
-            self.delay_start = time
-            #increment the amount of peds waiting
-            #add the impatient event after a minute of at the button
-            heappush(event_list, events.ped_event("impatient", time + 60, self.id))
-            if will_press(ped.peds_waiting, ped.pushed, trace = ped.button_trace):
-                event_list = self.push_button(time, event_list, signal_left)
-            ped.peds_waiting += 1
-
-        if event == "impatient":
-            if self.walked:
-                #do nothing if already walked
-                return None
-            if will_press(ped.peds_waiting, ped.pushed, trace = ped.button_trace, impatient=True):
-                event_list = self.push_button(time, event_list, signal_left)
-        if event == "ped_exit":
-            #clear impatient event if it hasn't happened yet or just keep it in a skip it later if you want
-            return self.total_delay
-        self.last_time = time
+        
         return event_list
-#print("test")
-#test_event = events.event("test", 5)
-#print("worked")
-#test_list = events.event_list()
-#test_list.insert(test_event)
-#print(test_list)
-#events = [events.event("test", 20), events.event("test", 30), events.event("test", 60), events.event("test", 20), events.event("test", 1)]
-#for event in events:
-#    test_list.insert(event)
-#    print(test_list)
-#
-#for event in events:
-#    test_list.next()
-#    print(test_list)
 
+    def impatient_press(self, time, event_list, signal_left):
+        #is just impatient, deal with it, push the damn button
+        event_list = self.push_button(time, event_list, signal_left)
+        return event_list
+    
+    def at_signal(self, time, event_list, signal_left):
+        '''try to account for when they get there during signal plus additional press on being stranded? but also thats stupid like why would a pedestrian push the signal, which wouldnt do anything, if its already on'''
+        #skipped the button entirely
+        if self.walked:
+            return event_list
+        #is at the button
+        #pedestrian is now not moving and is waiting
+        self.delay_start = time
+        ped.peds_waiting += 1
+        #if they push the button push the button
+        if self.will_press():
+            event_list = self.push_button(time, event_list, signal_left)
 
-#test_list = events.event_list()
-#test_ped = ped(0, 'spawn', test_list, 0, .5, 'filler')
-#test_list = test_ped.update('y_exp', 100000, test_list, signal_left = 0)
-#print(test_ped.speed, test_ped.pos, test_ped.button_pos)
-#print(test_list)
+        return event_list
+
+    def crossing(self, time):
+        '''should only be called from within walk signal for peds who can make it across in the current cycle'''
+        '''peds_crossing should be updated outside of class once all peds have been iterated through'''
+        ped.peds_crossing += 1
+        self.walked = True
+        if self.at_button:
+           ped.peds_waiting -= 1
+        '''value to be grabbed'''
+        #if they never waited they had no delay
+        if self.delay_start == None:
+            self.total_delay = 0
+        #otherwise calculate the difference between when they started waiting and when they crossed
+        else:
+            self.total_delay = time - self.delay_start
+        #if the ped crossed remove them from the waiting heap make sure they only pop themselves
+        #assert heappop(ped.button_arrivals).id == self.id
+        
+        
+         
+        
+    def will_press(self):
+        '''never called directly'''
+        if ped.pushed:
+            return False
+        x = float(ped.button_trace.readline())
+        #peds waiting -1?
+        if ped.peds_waiting == 0 or self.stranded:
+            return Uniform(0, 16, x) <= 15
+        else:
+            return Uniform(0, ped.peds_waiting + 1, x) <= ped.peds_waiting
